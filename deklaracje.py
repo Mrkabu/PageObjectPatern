288eecff5d076def5bc4b1990aa4c7ad4e1f7df0
Mrkabu <sagan07@gmail.com>
2018-05-13 20:16:04 +0200

    Rozbudowano test1

--- deklaracje.py
+++ deklaracje.py
@@ -1,21 +1,25 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from Locators import Locators
+import unittest
 
-class Testy():
+class Testy(unittest.TestCase):
+
+    def __init__(self):
+        self.driver = webdriver.Chrome('E:\Python\Drivery\chromedriver.exe')
+        self.driver.maximize_window()
+        self.driver.get('https://ncplusgo.pl')
 
     def test1(self):
 
-        driver = webdriver.Chrome('E:\Python\Drivery\chromedriver.exe')
-        driver.maximize_window()
-        driver.get('https://ncplusgo.pl')
-        driver.find_element(By.XPATH, Locators.logo).click()
+        self.driver.find_element(By.XPATH, Locators.logo).click()
+        self.driver.find_element(By.XPATH, Locators.login).send_keys('login@login.pl')
+        self.driver.find_element(By.XPATH, Locators.password).send_keys('pass')
+        self.driver.find_element(By.XPATH, Locators.buttonzaloguj).click()
 
     def test2(self):
-        driver = webdriver.Chrome('E:\Python\Drivery\chromedriver.exe')
-        driver.maximize_window()
-        driver.get('https://ncplusgo.pl')
-        driver.find_element(By.XPATH, Locators.terazwtv).click()
+
+        self.driver.find_element(By.XPATH, Locators.terazwtv).click()
 
 run = Testy()
 run.test1()
