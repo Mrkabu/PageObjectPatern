095fedc4356cb2e1d2235df525e958bc7f493dfb
Mrkabu <sagan07@gmail.com>
2018-05-13 20:16:29 +0200

    Dodano locatory

--- Locators.py
+++ Locators.py
@@ -1,6 +1,11 @@
+from selenium import webdriver
+
 class Locators(object):
 
     logo = '//*[@id="top"]/div/div/div/div/nav/ul/li[2]/a'
     terazwtv = '//*[@id="top"]/div/div/div/div/div/nav/a[1]'
+    login = '//*[@id="Email"]'
+    password = '//*[@id="Password"]'
+    buttonzaloguj = '//*[@id="login-form"]/fieldset/div/div[1]/div[3]/button'
     #programtv = (By.XPATH, '/html/body/header/div/div/div/div/div/nav/a[2]')
     #zaloguj = (By.XPATH, '/html/body/header/div/div/div/div/nav/ul/li[3]/a')
