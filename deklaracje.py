7f614f928e1d53b77858511bbe309586c636ffd4
Mrkabu <sagan07@gmail.com>
2018-05-16 21:55:30 +0200

    zaloguj i wyszukaj - do uzupelnienia o logowanie

--- deklaracje.py
+++ deklaracje.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from Locators import Locators
+from selenium.webdriver.common.keys import Keys
+import time
 
 class Testy():
 
@@ -9,16 +11,26 @@ class Testy():
         self.driver.maximize_window()
         self.driver.get('https://ncplusgo.pl')
 
-    def test1(self):
+    def zalogujiwyszukaj(self):
 
         self.driver.find_element(By.XPATH, Locators.logo).click()
-        self.driver.find_element(By.XPATH, Locators.login).send_keys('login@login.pl')
-        self.driver.find_element(By.XPATH, Locators.password).send_keys('pass')
+        self.driver.find_element(By.XPATH, Locators.rozumiem).click()
+        #self.driver.find_element(By.XPATH, Locators.terazwtv).click()
+        self.driver.find_element(By.XPATH, Locators.login).send_keys('jakub.karol.kucharski@gmail.com')
+        self.driver.find_element(By.XPATH, Locators.password).send_keys('Hasloncplusgo@123')
         self.driver.find_element(By.XPATH, Locators.buttonzaloguj).click()
+        self.driver.find_element(By.XPATH, Locators.logowyszukiwarka).click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, Locators.wyszukiwarka).send_keys('Kruk' + Keys.ENTER)
+
+        k = self.driver.find_element(By.XPATH, Locators.kruk)
+
+        if k.is_displayed:
+            print("OK")
 
     def test2(self):
 
         self.driver.find_element(By.XPATH, Locators.terazwtv).click()
 
 run = Testy()
-run.test1()
+run.zalogujiwyszukaj()
