7e4a208adecd36cbb449c68f1e3c7baf6b34e9c9
Mrkabu <sagan07@gmail.com>
2018-05-17 22:33:47 +0200

    Dodano nowe obiekty

--- Locators.py
+++ Locators.py
@@ -5,5 +5,9 @@ class Locators(object):
     login = '//*[@id="Email"]'
     password = '//*[@id="Password"]'
     buttonzaloguj = '//*[@id="login-form"]/fieldset/div/div[1]/div[3]/button'
+    logowyszukiwarka = '//*[@id="top"]/div/div/div/div/nav/ul/li[1]/a/img'
+    wyszukiwarka = '//*[@id="search"]'
+    rozumiem = '//*[@id="Account_Login"]/div[2]/div/div/div/a[2]'
+    kruk = '//*[@id="Search_Search"]/main/div/div[2]/ul/li[1]/div/div[2]/h2/a'
     #programtv = (By.XPATH, '/html/body/header/div/div/div/div/div/nav/a[2]')
     #zaloguj = (By.XPATH, '/html/body/header/div/div/div/div/nav/ul/li[3]/a')
